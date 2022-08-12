3f1d6c917a0660d1925168c0aa2fa6459f578a82
matteozambra <matteo.zambra1@gmail.com>
2022-08-12 12:45:50 +0200

    state concat improved

--- MODEL/main.py
+++ MODEL/main.py
@@ -200,12 +200,16 @@ class Experiment:
         perf_dict = {'mse' : test_loss.item()}
         
         # save reports and reconstructions in the proper target directory
-        self.path_manager.save_configfiles(self.cparams, 'config_params')        
+        if not self.versioning:
+            self.path_manager.save_configfiles(self.cparams, 'config_params')
+            self.path_manager.save_model_output(lit_model.get_saved_samples(),
+                                                *lit_model.get_learning_curves())
+            lit_model.remove_saved_outputs()
+            self.path_manager.save_litmodel_trainer(lit_model, trainer)
+        #end
+        
         self.path_manager.print_evalreport(perf_dict)
-        self.path_manager.save_model_output(lit_model.get_saved_samples(),
-                                            *lit_model.get_learning_curves())
-        lit_model.remove_saved_outputs()
-        self.path_manager.save_litmodel_trainer(lit_model, trainer)
+        
         
     #end
 #end
