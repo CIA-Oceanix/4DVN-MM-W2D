1e064f1753482bbfd822430bfd47587c3164f0c8
matteozambra <matteo.zambra1@gmail.com>
2022-08-18 12:49:08 +0200

    regularize loss

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -458,10 +458,10 @@ class LitModel(pl.LightningModule):
         loss_lr = self.loss_fn( (reco_lr - data_lr),  mask = None )
         loss_hr = self.loss_fn( (reco_tot - data_hr),  mask = None ) #+ \
                   # self.loss_fn( (data_hr - reco_tot), mask = None )
-        regularization = self.loss_fn((reco_tot - self.Phi(reco_tot)), mask = None)
+        # regularization = self.loss_fn((reco_tot - self.Phi(reco_tot)), mask = None)
         
         loss = self.hparams.weight_lres * loss_lr + self.hparams.weight_hres * loss_hr
-        loss = loss + regularization
+        # loss = loss + regularization
         
         return dict({'loss' : loss}), outputs
     #end
