70841a2b7a9916839687937260de6f498abb3af5
matteozambra <matteo.zambra1@gmail.com>
2022-08-22 13:45:40 +0200

    mask loss

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -510,8 +510,8 @@ class LitModel(pl.LightningModule):
         reco_hr = outputs[:,24:,:,:]
         # anomaly = data_hr - data_lr
         reco_tot = reco_lr + reco_hr
-        loss_lr = self.loss_fn( (reco_lr - data_lr),  mask = None )
-        loss_hr = self.loss_fn( (reco_tot - data_hr),  mask = None )
+        loss_lr = self.loss_fn( (reco_lr - data_lr),  mask = mask_loss )
+        loss_hr = self.loss_fn( (reco_tot - data_hr),  mask = mask_loss )
         
         loss = self.hparams.weight_lres * loss_lr + self.hparams.weight_hres * loss_hr
                 
