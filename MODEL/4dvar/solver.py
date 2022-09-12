cedc890e8677fe4e8380f7197a9a10061cf551dc
matteozambra <matteo.zambra1@gmail.com>
2022-09-12 17:49:36 +0200

    split U in 3 chunks

--- MODEL/4dvar/solver.py
+++ MODEL/4dvar/solver.py
@@ -535,9 +535,8 @@ class Solver_Grad_4DVarNN(nn.Module):
     def var_cost(self, x, yobs, mask):
         
         data_fidelty = self.model_H(x, yobs, mask)
-        print('Data fidelty in var_cost = ', data_fidelty[0].mean(), data_fidelty[1].mean(), data_fidelty[2].mean())
         regularization = x - self.Phi(x)
-        print('Regularization in var_cost = ', regularization.mean())
+        print('in var_cost = ', data_fidelty[0].mean(), data_fidelty[1].mean(), data_fidelty[2].mean(), regularization.mean())
         
         var_cost = self.model_VarCost(data_fidelty, regularization, mask)
         var_cost_grad = torch.autograd.grad(var_cost, x, create_graph = True)[0]
