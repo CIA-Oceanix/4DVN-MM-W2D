a22c7179df53291fd45628f56d89fe116ecc4612
matteozambra <matteo.zambra1@gmail.com>
2022-09-30 10:12:02 +0200

    simple net and 20 channels in the hidden layer

--- MODEL/utls/dutls.py
+++ MODEL/utls/dutls.py
@@ -123,25 +123,25 @@ class W2DSimuDataModule(pl.LightningDataModule):
     
     def train_dataloader(self):
         return DataLoader(self.train_dataset,
-                          batch_size = self.batch_size)#,
-                           # generator = torch.Generator(device = DEVICE),
-                          # shuffle = True,
+                          batch_size = self.batch_size,
+                          generator = torch.Generator(device = DEVICE),
+                          shuffle = True)
                           # num_workers = WORKERS)
     #end
     
     def val_dataloader(self):
         return DataLoader(self.val_dataset,
-                          batch_size = self.batch_size)#,
-                           # generator = torch.Generator(device = DEVICE),
-                          # shuffle = False,
+                          batch_size = self.batch_size,
+                          generator = torch.Generator(device = DEVICE),
+                          shuffle = False)
                           # num_workers = WORKERS)
     #end
     
     def test_dataloader(self):
         return DataLoader(self.test_dataset,
-                          batch_size = self.batch_size)#,
-                           # generator = torch.Generator(device = DEVICE),
-                          # shuffle = False,
+                          batch_size = self.batch_size,
+                          generator = torch.Generator(device = DEVICE),
+                          shuffle = False)
                           # num_workers = WORKERS)
     #end
 #end
