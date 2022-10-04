modif in loss computation fixed point

MODEL/4dvar/dlmodels.py
@@ -388,8 +388,6 @@ class LitModel(pl.LightningModule):
         elif mode == 'points':
             
             mask = torch.zeros(data_shape)
-            # points_x = [4, 4, 30, 30]
-            # points_y = [4, 30, 4, 30]
             points_x = np.random.randint(0, data_shape[-2], 10)
             points_y = np.random.randint(0, data_shape[-1], 10)
             mask[:,:,points_x, points_y] = 1.
@@ -439,8 +437,8 @@ class LitModel(pl.LightningModule):
             
             if self.hparams.fixed_point:
                 outputs = self.Phi(input_data)
-                # reco_lr = data_lr.clone()
-                reco_lr = outputs[:,:24,:,:]
+                reco_lr = data_lr.clone()
+                # reco_lr = outputs[:,:24,:,:]
                 reco_hr = outputs[:,48:,:,:]
                 reco = data_lr + reco_hr
             else:
@@ -498,6 +496,10 @@ class LitModel(pl.LightningModule):
         
         loss = torch.stack([out['loss'] for out in outputs]).mean()
         self.save_epoch_loss(loss, self.current_epoch, 'train')
+        
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+        #end
     #end
     
     def validation_step(self, batch, batch_idx):

MODEL/utls/pathmng.py
@@ -53,6 +53,7 @@ class PathManager:
             os.mkdir(path_modeloutput)
         #end
         
+        self.model_name            = model_name
         self.path_ckpt             = path_ckpt
         self.path_litmodel_trainer = path_litmodel_trainer
         self.path_evalmetrics      = path_evalmetrics
@@ -105,6 +106,11 @@ class PathManager:
                                configfile_name + '.json'), 'w') as f:
             json.dump(configfile._asdict(), f, indent = 4)
         f.close()
+        
+        with open(os.path.join(self.path_configfiles,
+                               'modelname.txt'), 'w') as f:
+            f.write(self.model_name)
+        f.close()
     #end
     
     def save_litmodel_trainer(self, lit_model, trainer):
@@ -119,7 +125,7 @@ class PathManager:
     #end
     
     def save_model_output(self, outputs, cparams, train_losses, val_losses):
-                    
+        
         img_dim = outputs[0]['reco'].shape[-2:]
         data = torch.cat([item['data'] for item in outputs], dim = 0)
         reco = torch.cat([item['reco'] for item in outputs], dim = 0)