1498c5b498064cbde1108653f3f50c1d26ed21f9
matteozambra <matteo.zambra1@gmail.com>
2022-10-11 21:18:02 +0200

    deeper network

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -87,10 +87,10 @@ class ResNet(nn.Module):
 #end
 
 class Block(nn.Sequential):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, kernel_size, padding):
         super(Block, self).__init__(
-                nn.Conv2d(in_channels, out_channels, (5,5), 
-                          padding = 2, # or 'same'
+                nn.Conv2d(in_channels, out_channels, kernel_size, 
+                          padding = padding, # or 'same'
                           # padding_mode = 'reflect',
                           bias = True),
                 # nn.BatchNorm2d(out_channels),
@@ -109,10 +109,10 @@ class ConvNet(nn.Module):
         
         self.net = nn.Sequential(
             collections.OrderedDict([
-                ('block1', Block(ts_length, 32)),
-                ('block2', Block(32, 64)),
-                ('block3', Block(64, 128)),
-                ('adjlayer', nn.Conv2d(128, ts_length, (5,5), padding = 2, bias = True))
+                ('block1', Block(ts_length, 32, 5, 2)),
+                ('block2', Block(32, 64, 5, 2)),
+                ('block3', Block(64, 128, 7, 3)),
+                ('adjlayer', nn.Conv2d(128, ts_length, (7,7), padding = 3, bias = True))
             ])
         )
     #end
