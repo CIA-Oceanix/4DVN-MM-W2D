c93df7a654e14fb63a859627275d7d95ed8027ef
matteozambra <matteo.zambra1@gmail.com>
2022-10-12 13:38:01 +0200

    debug gradient clip

--- MODEL/main.py
+++ MODEL/main.py
@@ -207,9 +207,7 @@ class Experiment:
         
         # TRAINER : configure properties and callbacks
         profiler_kwargs = {'max_epochs'              : self.cparams.EPOCHS, 
-                           'log_every_n_steps'       : 1,
-                           'gradient_clip_value'     : 1,
-                           'gradient_clip_algorithm' : 'value'}
+                           'log_every_n_steps'       : 1}
         
         if torch.cuda.is_available():
             profiler_kwargs.update({'gpus'        : gpus})
