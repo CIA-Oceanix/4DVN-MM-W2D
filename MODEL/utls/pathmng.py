e553dfde1c2137244b6811dcc91a381cd9ca7de3
matteozambra <matteo.zambra1@gmail.com>
2022-11-08 19:28:43 +0100

    baseline in test improvement

--- MODEL/utls/pathmng.py
+++ MODEL/utls/pathmng.py
@@ -247,6 +247,8 @@ class PathManager:
         data = torch.cat([item['data'] for item in outputs], dim = 0)
         reco = torch.cat([item['reco'] for item in outputs], dim = 0)
         
+        print(data.__class__, reco.__class__, mask_land.__class__)
+        
         reco_ncd = nc.Dataset(os.path.join(self.path_modeloutput, 'reconstructions.nc'), 'a')
         if run == 0:
             reco_ncd['data'][0,:,:,:,:] = data
