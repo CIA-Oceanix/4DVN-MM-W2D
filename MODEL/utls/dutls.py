710ee5a2a190802307999e7d238b95cef920562c
matteozambra <matteo.zambra1@gmail.com>
2022-12-03 15:09:51 +0100

    num workers

--- MODEL/utls/dutls.py
+++ MODEL/utls/dutls.py
@@ -10,7 +10,7 @@ from torch.utils.data import Dataset, DataLoader
 
 if torch.cuda.is_available():
     DEVICE  = torch.device('cuda')
-    WORKERS = 64
+    WORKERS = 32
 else:
     DEVICE  = torch.device('cpu')
     WORKERS = 8
