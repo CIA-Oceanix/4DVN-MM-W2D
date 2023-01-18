3bd94af7285f202a22c2d86b49d9a91efb4fd7a7
matteozambra <matteo.zambra1@gmail.com>
2023-01-18 16:48:59 +0100

    light unet

--- MODEL/4dvar/unet.py
+++ MODEL/4dvar/unet.py
@@ -537,18 +537,18 @@ class UNet4(nn.Module):
         self.n_classes = n_classes
         self.bilinear = bilinear
         
-        self.inc = DoubleConv(n_channels, 64)
-        self.down1 = Down(64, 128)
-        self.down2 = Down(128, 256)
+        self.inc = DoubleConv(n_channels, 128)
+        self.down1 = Down(128, 128)
+        # self.down2 = Down(128, 256)
         #self.down3 = Down(256, 512)
-        factor = 2 if bilinear else 1
+        # factor = 2 if bilinear else 1
         
         #self.down4 = Down(512, 1024 // factor)
         #self.up1 = Up(1024, 512 // factor, bilinear)
         #self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        # self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 128, bilinear)
+        self.outc = OutConv(128, n_classes)
         
     def forward(self, x):
         x1 = self.inc(x)
