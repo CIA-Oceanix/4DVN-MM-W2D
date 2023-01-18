b572bf1240ce74255e683b9516f13f63ecfd3552
matteozambra <matteo.zambra1@gmail.com>
2023-01-18 17:23:26 +0100

    light unet

--- MODEL/4dvar/unet.py
+++ MODEL/4dvar/unet.py
@@ -22,7 +22,11 @@ class DoubleConv(nn.Module):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
-            
+        
+        self.in_channels  = in_channels
+        self.out_channels = out_channels
+        self.mid_channels = mid_channels
+        
         if activation == 'relu':
             self.double_conv = nn.Sequential(
                     nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False,padding_mode=padding_mode),
@@ -240,6 +244,8 @@ class Up(nn.Module):
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
+        
+        self.corrective_conv = nn.Conv2d(192, 128, 3, padding = 'same')
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -253,6 +259,7 @@ class Up(nn.Module):
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
+        x = self.corrective_conv(x)
         return self.conv(x)
 
 class Up_1D(nn.Module):
