8e5dcdd443c07c04b6cb20f81f63146e61a28c78
matteozambra <matteo.zambra1@gmail.com>
2023-01-30 18:17:18 +0100

    dim_obs = 2 for only-time-series multi-modal term

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -433,15 +433,19 @@ class ModelObs_MM1d(nn.Module):
         in_channels = timesteps
         
         self.net_state = nn.Sequential(
-            nn.Conv1d(in_channels, 128, kernel_size = 5, padding = 'same'),
+            nn.Conv1d(in_channels, 64, kernel_size = 5, padding = 'same'),
             nn.LeakyReLU(0.1),
-            nn.Conv1d(128, in_channels, kernel_size = 5, padding = 'same')
+            nn.Conv1d(64, 128, kernel_size = 3, padding = 'same'),
+            nn.LeakyReLU(0.1),
+            nn.Conv1d(128, in_channels, kernel_size = 3, padding = 'same')
         )
         
         self.net_data = nn.Sequential(
             nn.Conv1d(in_channels, 128, kernel_size = 5, padding = 'same'),
             nn.LeakyReLU(0.1),
-            nn.Conv1d(128, in_channels, kernel_size = 5, padding = 'same')
+            nn.Conv1d(128, in_channels, kernel_size = 3, padding = 'same'),
+            nn.LeakyReLU(0.1),
+            nn.Conv1d(128, in_channels, kernel_size = 3, padding = 'same')
         )
     #end
     
