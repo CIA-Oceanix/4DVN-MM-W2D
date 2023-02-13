df82972352754226129350da2bc0204823111b94
matteozambra <matteo.zambra1@gmail.com>
2023-02-14 00:12:55 +0100

    simple stdmean normalization

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -1566,7 +1566,6 @@ class LitModel_OSSE1_WindComponents(LitModel_Base):
                 outputs = self.Phi(input_data)
                 print(outputs.mean())
                 reco_lr = data_lr_input.clone()
-                outputs = torch.autograd.Variable(torch.zeros(reco_lr.shape))
                 reco_hr = reco_lr + outputs
             #end
         #end
