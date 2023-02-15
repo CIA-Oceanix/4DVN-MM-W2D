e348538b59d549a7847700cc2472101871cc39ad
matteozambra <matteo.zambra1@gmail.com>
2023-02-15 17:12:04 +0100

    explicit cast to float32 in compute loss

--- MODEL/utls/dutls.py
+++ MODEL/utls/dutls.py
@@ -383,17 +383,17 @@ class W2DSimuDataModule(pl.LightningDataModule):
     
     def train_dataloader(self):
         
-        return DataLoader(self.train_dataset, batch_size = self.batch_size, shuffle = False)
+        return DataLoader(self.train_dataset, batch_size = self.batch_size, generator = torch.Generator(device = DEVICE))
     #end
     
     def val_dataloader(self):
         
-        return DataLoader(self.val_dataset, batch_size = self.batch_size, shuffle = False)
+        return DataLoader(self.val_dataset, batch_size = self.batch_size, generator = torch.Generator(device = DEVICE))
     #end
     
     def test_dataloader(self):
         
-        return DataLoader(self.test_dataset, batch_size = self.batch_size, shuffle = False)
+        return DataLoader(self.test_dataset, batch_size = self.batch_size, generator = torch.Generator(device = DEVICE))
     #end
 #end
 
