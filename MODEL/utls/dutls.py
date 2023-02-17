be4f4967fb8b6a9c715053179ec588b4d234219d
matteozambra <matteo.zambra1@gmail.com>
2023-02-17 09:53:47 +0100

    try again parallelization

--- MODEL/utls/dutls.py
+++ MODEL/utls/dutls.py
@@ -385,17 +385,17 @@ class W2DSimuDataModule(pl.LightningDataModule):
     #end
     
     def train_dataloader(self):
-        return DataLoader(self.train_dataset, batch_size = self.batch_size)
+        return DataLoader(self.train_dataset, batch_size = self.batch_size, generator = torch.Generator(DEVICE))
     #end
     
     def val_dataloader(self):
         
-        return DataLoader(self.val_dataset, batch_size = self.batch_size)
+        return DataLoader(self.val_dataset, batch_size = self.batch_size, generator = torch.Generator(DEVICE))
     #end
     
     def test_dataloader(self):
         
-        return DataLoader(self.test_dataset, batch_size = self.batch_size)
+        return DataLoader(self.test_dataset, batch_size = self.batch_size, generator = torch.Generator(DEVICE))
     #end
 #end
 
