5bc0f3627616e176d857547526030a6731713949
matteozambra <matteo.zambra1@gmail.com>
2023-03-03 20:58:03 +0100

    towards a most robust and flexible framework

--- MODEL/4dvar/litmodels.py
+++ MODEL/4dvar/litmodels.py
@@ -405,7 +405,7 @@ class LitModel_OSSE1_WindModulus(LitModel_Base):
         data_hr_u, data_hr_v = data[0], data[1]
         data_hr_gt = (data_hr_u.pow(2) + data_hr_v.pow(2)).sqrt()
         
-        if False:
+        if True:
             # Downsample to obtain ERA5-like data
             data_lr_gt = self.spatial_downsample_interpolate(data_hr_gt)
         else:
@@ -428,7 +428,7 @@ class LitModel_OSSE1_WindModulus(LitModel_Base):
         #end
         
         # Alternative : persistence models
-        if True:
+        if False:
             data_lr_obs = data_lr_obs.clone()
             data_hr_obs = data_hr_gt.clone()
         else:
