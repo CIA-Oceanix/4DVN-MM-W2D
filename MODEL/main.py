1e5bb1914251b824dae319148ce3c01a4e3f52f5
matteozambra <matteo.zambra1@gmail.com>
2023-03-06 09:31:03 +0100

    increase split size mb

--- MODEL/main.py
+++ MODEL/main.py
@@ -2,7 +2,7 @@ import os
 import sys
 sys.path.append(os.path.join(os.getcwd(), 'utls'))
 sys.path.append(os.path.join(os.getcwd(), '4dvar'))
-os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:1024'
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:2048'
 
 from dotenv import load_dotenv
 import datetime
