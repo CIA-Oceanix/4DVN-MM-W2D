dcc0dbcb7ddaa386299c0dc97ddf0bb8b8bab343
matteozambra <matteo.zambra1@gmail.com>
2023-03-22 12:34:46 +0100

    4DVarNet model_H MM for C3 and components

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -481,7 +481,12 @@ class ModelObs_MM_uv(ModelObs_MM_mod):
         dy_modulus = ModelObs_MM_mod.forward(self, x[:,:72], y_obs[:,:72], mask[:,:72])
         
         # Inclusion of angle data/state
-        x_theta_spatial = torch.atan2(x[:,144:168], x[:,72:96]) + torch.atan2(x[:,168:192], x[:,96:120])
+        try:
+            x_theta_spatial = torch.atan2(x[:,144:168], x[:,72:96]) + torch.atan2(x[:,168:192], x[:,96:120])
+        except:
+            x_theta_spatial = torch.atan2(x[:,144:168], x[:,72:96])
+        #end
+        
         y_mwind_spatial = (y_obs[:,:24] + y_obs[:,24:48]).mul(mask[:,24:48])
         
         # feature maps
