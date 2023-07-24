d5ccfaa96f8ddebc8c26eb1cd6a9336db4a84d1d
matteozambra <matteo.zambra1@gmail.com>
2023-07-24 14:11:35 +0200

    lighter unet pdf

--- MODEL/utls/metrics.py
+++ MODEL/utls/metrics.py
@@ -161,8 +161,8 @@ class KLDivLoss(torch.nn.Module):
         
         target[target < 1e-9] = 1e-9
         output[output < 1e-9] = 1e-9
-        kld = target.mul( target.log() - output.log() )
-        kld = kld.sum() / (target.shape[0] * target.shape[1])
+        kld = target.mul( target.log() - output.log() ).sum(dim = -1)
+        kld = kld.mean()
         return kld
     #end
 #end
