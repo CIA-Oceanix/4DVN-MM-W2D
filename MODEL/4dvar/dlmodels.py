fe12a63d4fcd93741d565ae44798b2f8611feb25
matteozambra <matteo.zambra1@gmail.com>
2023-07-28 15:39:13 +0200

    depthwise conv2d for unet histogrammization part

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -364,7 +364,7 @@ class UNet1_pdf(nn.Module):
             nn.ReLU()
         )
         self.downsample = nn.AvgPool2d(cparams.LR_KERNELSIZE)
-        self.linear     = nn.Conv2d(out_channels, shape_data[1] * shape_data[-1])
+        self.linear     = nn.Conv2d(out_channels, shape_data[1] * shape_data[-1], kernel_size = 3, padding = 1)
         self.normalize  = nn.LogSoftmax(dim = -1)
     #end
     
