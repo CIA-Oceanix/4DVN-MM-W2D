8c9bb2d36427ab84228a8297731caf8154cb7a28
matteozambra <matteo.zambra1@gmail.com>
2023-07-28 12:04:30 +0200

    depthwise conv2d for unet histogrammization part

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -357,8 +357,8 @@ class UNet1_pdf(nn.Module):
         
         # Histogrammization
         self.out_conv   = nn.Sequential(
-            # DepthwiseConv2d(in_channels, out_channels, kernel_size = (3,3), padding = 1),
-            # nn.ReLU(),
+            DepthwiseConv2d(out_channels, out_channels, kernel_size = (3,3), padding = 1),
+            nn.ReLU(),
             DepthwiseConv2d(out_channels, 512, kernel_size = (5,5), padding = 2),
             nn.ReLU(),
             DepthwiseConv2d(512, shape_data[1] * shape_data[-1], kernel_size = (5,5), padding = 2),
