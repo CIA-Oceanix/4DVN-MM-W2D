85d56b7726dd925113d66a1a886826f749317098
matteozambra <matteo.zambra1@gmail.com>
2023-07-29 19:39:26 +0200

    new reshape try

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -360,9 +360,8 @@ class UNet1_pdf(nn.Module):
             nn.ReLU(),
             nn.Conv2d(256, 256, kernel_size = (5,5), padding = 2),
             nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size = (5,5), padding = 2),
+            nn.Conv2d(256, out_channels, kernel_size = (3,3), padding = 1),
             nn.ReLU(),
-            nn.Conv2d(256, out_channels, kernel_size = (3,3), padding = 1)
         )
         # self.downsample = nn.AvgPool2d(cparams.LR_KERNELSIZE)
         self.downsample = nn.Sequential(
