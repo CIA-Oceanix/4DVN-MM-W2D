cc14e20016fe1036885c7fd2d9360ad7920bff0f
matteozambra <matteo.zambra1@gmail.com>
2023-07-30 14:20:28 +0200

    this works!

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -356,9 +356,9 @@ class UNet1_pdf(nn.Module):
         
         # Histogrammization
         self.to_hist    = nn.Sequential(
-            nn.Conv2d(in_channels, 256, kernel_size = (5,5), padding = 2),
+            nn.Conv2d(in_channels, 512, kernel_size = (5,5), padding = 2),
             nn.ReLU(),
-            nn.Conv2d(256, out_channels, kernel_size = (5,5), padding = 2),
+            nn.Conv2d(512, out_channels, kernel_size = (5,5), padding = 2),
             nn.ReLU(),
             nn.Conv2d(out_channels, out_channels, kernel_size = (3,3), padding = 1),
             # nn.ReLU(),
