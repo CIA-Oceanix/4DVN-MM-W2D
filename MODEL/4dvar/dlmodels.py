bf511100b20fbf852b10b0582de2e10bc46212fd
matteozambra <matteo.zambra1@gmail.com>
2023-09-12 18:17:34 +0200

    see if plain config works

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -253,8 +253,10 @@ class HistogrammizationDirect(nn.Module):
         # Residual block
         wind_hist_empirical = fs.fieldsHR2hist(data_fields_hr.clone().detach().cpu(), self.lr_kernelsize, self.wind_bins, progbars = True, verbose = False)
         wind_hist_empirical = torch.autograd.Variable(wind_hist_empirical, requires_grad = True).to(DEVICE)
+        print(wind_hist_empirical)
         out_res  = out + torch.log(wind_hist_empirical)
         out_norm = self.normalize(out_res)
+        print(out_norm)
         
         return out_norm
     #end
