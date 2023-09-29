10da1f11e319da5a5c15a82559de21220bfd40a8
matteozambra <matteo.zambra1@gmail.com>
2023-09-29 15:39:50 +0200

    lighter model selectiin

--- MODEL/4dvar/dlmodels.py
+++ MODEL/4dvar/dlmodels.py
@@ -385,7 +385,8 @@ class TrainableFieldsToHist(nn.Module):
     #end
     
     def get_high_resolution(self, fields_in, fields_out):
-        fields_lr_intrp = self.interpolate_lr(fields_in[:,:self.timesteps,:,:], self.lr_sfreq)
+        fields_ = fields_out[:,:24,:,:] + fields_out[:,48:,:,:]
+        fields_lr_intrp = self.interpolate_lr(fields_[:,:self.timesteps,:,:], self.lr_sfreq)
         fields_anomaly  = fields_out[:, 2 * self.timesteps:, :,:]
         fields_hr = fields_anomaly + fields_lr_intrp
         return fields_hr, fields_lr_intrp, fields_anomaly
